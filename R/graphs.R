fa655008e746da9078eca7c5f711d4391d3e20bd
Gergely Daróczi <gergely@snowl.net>
2012-01-04 00:26:10 +0100

    stars added to rp.cor.plot

--- R/graphs.R
+++ R/graphs.R
@@ -527,15 +527,23 @@ rp.cor.plot <- function(x, lower.panel='panel.smooth', upper.panel='panel.cor', 
 		## generating color from given palette
 		col <- rp.palette(1, theme, colorize)
         ## panels
-        panel.cor <- function(x, y, digits=2, prefix="", cex.cor, ...)
+        panel.cor <- function(x, y, digits=2, prefix="", cex.cor) 
         {
-            usr <- par("usr"); on.exit(par(usr))
-            par(usr = c(0, 1, 0, 1))
-            r <- cor(x, y)  #r <- abs(cor(x, y))
-            txt <- format(c(r, 0.123456789), digits=digits)[1]
-            txt <- paste(prefix, txt, sep="")
-            if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
-            text(0.5, 0.5, txt, cex = cex.cor * abs(r))
+            usr <- par("usr"); on.exit(par(usr)) 
+            par(usr = c(0, 1, 0, 1)) 
+            r <- cor(x, y) 
+            txt <- format(c(r, 0.123456789), digits=digits)[1] 
+            txt <- paste(prefix, txt, sep="") 
+            if(missing(cex.cor)) cex <- 0.8/strwidth(txt) 
+            
+            test <- cor.test(x,y) 
+            # borrowed from printCoefmat
+            Signif <- symnum(test$p.value, corr = FALSE, na = FALSE, 
+                    cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                    symbols = c("***", "**", "*", ".", " ")) 
+            
+            text(0.5, 0.5, txt, cex = cex * abs(r) * 1.2)
+            text(.8, .8, Signif, cex=cex, col=2) 
         }
         panel.hist <- function(x, ...)
         {
