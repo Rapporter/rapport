2bca39bcd8b88a7f56fb917e0becdefc03754701
Gergely Daróczi <gergely@snowl.net>
2012-02-04 19:58:11 +0100

    demo update: add HTML export functionality

--- demo/rapport.R
+++ demo/rapport.R
@@ -7,7 +7,7 @@ readline('Press ENTER to continue!')
 tpl.meta('example')$example
 
 
-## Let us run the first example of 'example.tpl'
+## Let us run the first example of 'example.tpl'.
 ## Will run: tpl.example('example', 1)
 ## Equivalent to running the first command of the above listed.
 readline('Press ENTER to continue!')
@@ -18,15 +18,24 @@ tpl.example('example', 1)
 readline('Press ENTER to continue!')
 tpl.list()
 
-## Ok, 'correlations.tpl' seems interesting, let us run the example of that!
-## Will run: tpl.example('correlations')
+## Ok, 'anova.tpl' seems interesting, let us run the example of that!
+## Will run: tpl.example('anova', 'all')
 readline('Press ENTER to continue!')
-tpl.example('correlations')
+tpl.example('anova', 'all')
+
+## And check out the HTML export of that *rapport*!
+## Will run: tpl.export(tpl.example('anova', 'all'))
+readline('Press ENTER to continue!')
+tpl.export(tpl.example('anova', 'all'))
 
 ## Or let us run this template against our custom data:
-## Will run: rapport('correlations', data=mtcars, vars=c('hp', 'mpg', 'wt')) 
+## Will run: rapport('anova', data=ius2008, resp = 'edu', fac = 'dwell') 
 readline('Press ENTER to continue!')
-rapport('correlations', data=mtcars, vars=c('hp', 'mpg', 'wt'))
+rapport('anova', data=ius2008, resp = 'edu', fac = 'dwell')
 
-## Do not forget to check out the docs located @ \url{http://al3xa.github.com/rapport/}.
+## And an easy wrapper to get the HTML export of the document in one run:
+## Will run: rapport.html('anova', data=ius2008, resp = 'edu', fac = 'dwell') 
+rapport.html('anova', data=ius2008, resp = 'edu', fac = 'dwell')
+
+## Do not forget to check out the docs located @ \url{http://rapport-package.info}.
 ## Thanks for your kind attention!
