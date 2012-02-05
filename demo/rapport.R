3283182594b4480bcbe9fc43b2ab84ea6dc8ad3b
Gergely Daróczi <gergely@snowl.net>
2012-02-05 13:32:10 +0100

    demo: add spaces before cursor + according NEWS update

--- demo/rapport.R
+++ demo/rapport.R
@@ -17,7 +17,7 @@ require(rapport)
 ## To make this easy, check out the example commands of different templates, eg.:
 ## Will run:
 ##      tpl.meta('example')$example
-readline('Press ENTER to continue!')
+readline('Press ENTER to continue! ')
 tpl.meta('example')$example
 
 
@@ -26,39 +26,39 @@ tpl.meta('example')$example
 ##      tpl.example('example', 1)
 ## Which is equivalent to running the first command of the above listed:
 ##      rapport("example", ius2008, var='leisure')
-readline('Press ENTER to continue!')
+readline('Press ENTER to continue! ')
 tpl.example('example', 1)
 
 ## And find out other templates too!
 ## Will run:
 ##      tpl.list() 
-readline('Press ENTER to continue!')
+readline('Press ENTER to continue! ')
 tpl.list()
 
 ## Ok, 'correlations.tpl' seems interesting, let us run an example of that!
 ## Will run:
 ##      tpl.example('correlations')
-readline('Press ENTER to continue!')
+readline('Press ENTER to continue! ')
 tpl.example('correlations')
 
 ## And check out the HTML export of *all* examples!
 ## Will run:
 ##      tpl.export(tpl.example('correlations', 'all'))
 ## NOTE: this needs pandoc to be installed!
-readline('Press ENTER to continue!')
+readline('Press ENTER to continue! ')
 tpl.export(tpl.example('correlations', 'all'))
 
 ## Or let us run this template against our custom data:
 ## Will run:
 ##      rapport('correlations', data=mtcars, vars=c('mpg', 'hp', 'wt', 'qsec')) 
-readline('Press ENTER to continue!')
+readline('Press ENTER to continue! ')
 rapport('correlations', data=mtcars, vars=c('mpg', 'hp', 'wt', 'qsec')) 
 
 ## And an easy wrapper to get the HTML export of the document in one run with hi-res images attached:
 ## Will run:
 ##      rapport.html('correlations', data=mtcars, vars=c('mpg', 'hp', 'wt', 'qsec'), graph.hi.res = TRUE) 
 ## NOTE: this needs pandoc to be installed!
-readline('Press ENTER to continue!')
+readline('Press ENTER to continue! ')
 rapport.html('correlations', data=mtcars, vars=c('mpg', 'hp', 'wt', 'qsec'), graph.hi.res = TRUE)
  
 ## Do not forget to check out the docs:
