7b198538fe01028bffad235c3f7782b444b4b6d5
Gergely Daróczi <gergely@snowl.net>
2012-11-07 00:55:46 +0100

    fix: only quote variable names if there is any

--- R/rp_helpers.R
+++ R/rp_helpers.R
@@ -728,7 +728,11 @@ check.type <- function(x){
 #' fml("hp", c("am", "cyl"))    # "hp ~ am + cyl"
 #' @export
 fml <- function(left, right, join.left = ' + ', join.right = ' + '){
-    sprintf('%s ~ %s', paste(wrap(left, wrap = '`'), collapse = join.left), paste(wrap(right, wrap = '`'), collapse = join.right))
+    if (!identical(left, ''))
+        left <- wrap(left, wrap = '`')
+    if (!identical(right, ''))
+        right <- wrap(right, wrap = '`')
+    sprintf('%s ~ %s', paste(left, collapse = join.left), paste(right, collapse = join.right))
 }
 
 
