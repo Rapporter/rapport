421dedd9b8bc7928ab28c1328fbae3329aa2094b
Aleksandar Blagotić <aca.blagotic@gmail.com>
2013-01-09 01:35:06 +0100

    Gibberish commits beg for more sleep (or coffee FWIW)

--- R/rp_helpers.R
+++ R/rp_helpers.R
@@ -137,7 +137,7 @@ as.character.rp.inputs <- function(x, ...){
                        variable = paste(mandatory, x$type, limits, sep = ""),
                        stopf('Incorrect input type (%s)!', x$type)
                        )
-        paste(paste0(mandatory, x$name), opts, x$label, x$desc, sep = " | ")
+        paste(x$name, opts, x$label, x$desc, sep = " | ")
     }))
 }
 
