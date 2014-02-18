01840e13512977384d9fbe79c5721bf91c2b3549
Gergely Daróczi <gergely@snowl.net>
2014-02-18 14:42:21 +0100

    grammar nazi should have been also aware of the test cases, dammit

--- inst/tests/test-internals.R
+++ inst/tests/test-internals.R
@@ -6,15 +6,15 @@ tpl.no.head <- c("<!--head", "head-->")
 tpl.empty.head <- c("<!--head", "", " ", "\t", "\n", "head-->")
 tpl.no.body <- c("<!--head", "meta:", "  title: No body", "  author: Tester", "  description: Template with no body :'-(", "inputs:", "- name: c", "  class: 'character'", "  label: Character input", "  description: Character input", "  standalone: true", "head-->")
 
-no.body.err.msg.re <- "what good is a template if it has no body\\? http://bit\\.ly/11E5BQM"
+no.body.err.msg.re <- "What good is a template if it has no body\\? http://bit\\.ly/11E5BQM"
 
 
 context('Header tags')
 
-expect_error(rapport:::rapport.read(tpl.no.htag.1), 'opening header tag not found in first line')
-expect_error(rapport:::rapport.read(tpl.no.htag.2), 'closing header tag not found')
-expect_error(rapport:::rapport.read(tpl.no.head), 'template header not found')
-expect_error(rapport:::rapport.header(tpl.empty.head), 'template header is empty')
+expect_error(rapport:::rapport.read(tpl.no.htag.1), 'Opening header tag not found in first line.')
+expect_error(rapport:::rapport.read(tpl.no.htag.2), 'Closing header tag not found.')
+expect_error(rapport:::rapport.read(tpl.no.head), 'Template header not found.')
+expect_error(rapport:::rapport.header(tpl.empty.head), 'Template header is empty.')
 
 
 context('Template body presence')
